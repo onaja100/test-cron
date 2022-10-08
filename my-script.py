8a11b85fa39cb7372641940e051ad58571841e3e
sopon chimtin <onaja100@gmail.com>
2022-10-08 21:57:07 +0700

    second commit

--- my-script.py
+++ my-script.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
+from asyncio import constants
 import subprocess
   
 def sendmessage(message="test"):
     subprocess.Popen(['notify-send', message])
+    print('test')
     return
   
 if __name__ == '__main__':
